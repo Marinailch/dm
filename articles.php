2ae160477db6f895d3187ad1547e7739f81d2be0
Marinailch <marina_ilch@mail.ru>
2018-01-16 19:25:45 +0200

    foto

---
+++ articles.php
@@ -0,0 +1,243 @@
+<?php include "header.php" ?>
+<? if (isset($_GET['id'])) {
+    $id = strtolower(strip_tags(trim($_GET['id'])));
+  }
+  else{
+    $id='';
+  }?>
+
+
+
+
+
+
+
+<?php  switch($id){
+
+default:
+$img="b1";
+$title_h1="День меланомы 2017 пройдет 13 апреля";
+// $title="заголовок_глав11";
+// $discriptions="discriptions111";
+break;
+case 'b1':
+$img="b1";
+$title_h1="День меланомы 2017 пройдет 13 апреля";
+// $title="заголовок_глав22";
+// $discriptions="discriptions111";
+break;
+case 'b2':
+$img="b2";
+$title_h1="Малоинвазивная радиоволновая хирургия кожи аппаратом Surgitron ellman";
+// $title="заголовок_глав33";
+// $discriptions="discriptions111";
+break;
+case 'b3':
+$img="b3";
+$title_h1="ФРАКЦИОННЫЙ ФОТОТЕРМОЛИЗ - ФРАКСЕЛЬ / FRAXEL";
+break;
+case 'b4':
+$img="b4";
+$title_h1="Прессотерапия на аппарате Limpho-drain";
+break;
+case 'b5':
+$img="b5";
+$title_h1="ЗОЛОТОЙ ВЕК» АППАРАТОВ ЛАЗЕРНОЙ ЭПИЛЯЦИИ";
+break;
+case 'b6':
+$img="b6_1";
+$title_h1="Ведущие специалисте Клиники Евродерм в Днепре";
+break;
+case 'b7':
+$img="b7";
+$title_h1="Поздравляем наших докторов";
+break;
+case 'b8':
+$img="b8";
+$title_h1="";
+break;
+case 'b9':
+$img="b9";
+$title_h1="";
+break;
+case 'b10':
+$img="b10";
+$title_h1="";
+break;
+case 'b11':
+$img="b11";
+$title_h1="";
+break;
+case 'b12':
+$img="b12";
+$title_h1="";
+break;
+case 'b13':
+$img="b13";
+$title_h1="";
+break;
+case 'b15':
+$img="b14";
+$title_h1="";
+break;
+case 'b15':
+$img="b15";
+$title_h1="";
+break;
+case 'b16':
+$img="b16";
+$title_h1="";
+break;
+case 'b17':
+$img="b17";
+$title_h1="";
+break;
+case 'b18':
+$img="b18";
+$title_h1="";
+break;
+case 'b19':
+$img="b19";
+$title_h1="";
+break;
+case 'b20':
+$img="b20";
+$title_h1="";
+break;
+case 'b21':
+$img="b21";
+$title_h1="";
+break;
+case 'b22':
+$img="b22";
+$title_h1="";
+break;
+case 'b23':
+$img="b23";
+$title_h1="";
+break;
+case 'b24':
+$img="b24";
+$title_h1="";
+break;
+
+
+}
+?>
+    <div class="servise_page" >
+        <div class="container">
+        <h4 class="servise_page" style="background: none; margin-bottom: 20px;">Блог</h4>
+
+
+            <div style="margin-top: 20px;" id="b5">
+                 <img src="img/blog/<?php echo"$img";?>.jpg" style="float:left; margin-right: 20px; margin-bottom: 20px; max-width: 600px; width: 100%; " >
+<h1 style="    font-size: 20px;    font-weight: 600; margin-right: 20px;   color: #787878;}"><?php echo"$title_h1"?></h1>
+<p style="width: 100%;">
+
+
+ <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.8";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+
+<p  style="margin-top: -13px; margin-bottom: 28px; font-weight: 600; color: dimgrey"> 
+<div class="fb-share-button" data-href="http://dnipromedical.com/articles.php?id=<?php echo"$id"?>" data-layout="button_count" data-mobile-iframe="false">
+<a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://http://dnipromedical.com/articles.php?id=<?php echo"$id"?>">поделиться</a>
+</div>
+</p>
+
+<?php  switch($id){
+
+         default:
+          include 'pages/blog/b1.php';
+        break;
+        case 'b1':
+          include 'pages/blog/b1.php';
+        break;
+        case 'b2':
+          include 'pages/blog/b2.php';
+        break;
+        case 'b3':
+          include 'pages/blog/b3.php';
+        break;
+        case 'b4':
+          include 'pages/blog/b4.php';
+        break;
+        case 'b5':
+          include 'pages/blog/b5.php';
+        break;
+         case 'b6':
+          include 'pages/blog/b6.php';
+        break;
+         case 'b7':
+          include 'pages/blog/b7.php';
+        break;
+        case 'b8':
+          include 'pages/blog/b8.php';
+        break;
+        case 'b9':
+        include 'pages/blog/b9.php';
+        break;
+        case 'b10':
+        include 'pages/blog/b10.php';
+        break;
+        case 'b11':
+        include 'pages/blog/b11.php';
+        break;
+        case 'b12':
+        include 'pages/blog/b12.php';
+        break;
+        case 'b13':
+        include 'pages/blog/b13.php';
+        break;
+        case 'b14':
+        include 'pages/blog/b14.php';
+        break;
+        case 'b15':
+        include 'pages/blog/b15.php';
+        break;
+        case 's16':
+        include 'pages/blog/b16.php';
+        break;
+        case 's17':
+        include 'pages/blog/b17.php';
+        break;
+        case 's18':
+        include 'pages/blog/b18.php';
+        break;
+        case 's19':
+        include 'pages/blog/b19.php';
+        break;
+        case 'b20':
+        include 'pages/blog/b20.php';
+        break;
+        case 'b21':
+        include 'pages/blog/b21.php';
+        break;
+        case 'b22':
+        include 'pages/blog/b22.php';
+        break;
+        case 'b23':
+        include 'pages/blog/b23.php';
+        break;
+    case 'b24':
+        include 'pages/blog/b24.php';
+        break;
+        
+               // case 'shop':
+        //   include 'shop/admin/add2cat.php';
+            }
+  ?>
+</p>
+
+            </div>
+</div>
+</div>
+<?php include "footer.php" ?>
+
+  
---
+++ map.php
@@ -0,0 +1,33 @@
+    <!-- карта -->
+    <div id="map">
+         <iframe id=""  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.6660459338036!2d35.024687577733545!3d48.46293676764097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbe2e2476c6299%3A0x589a1edf3431caea!2z0JTQvdC10L_RgNC-LdCc0LXQtNC40LrQsNC7!5e0!3m2!1sru!2sru!4v1507291392315" width="100%" height="500" frameborder="0"   allowfullscreen></iframe>
+         <div id="map_frime" style="width: 100%; height: 500px;  position: relative; top: -500px;" onclick="bhiden();"></div>
+
+        <div class="map_info">
+            <a><img src="img/14.png" id="show_map" onclick="bshow();" ></a>
+            <a><img src="img/15.png" id="hiden_map" onclick="bhiden();" ></a>
+            <h3>часы работы</h3>
+            <p><span style="margin-right: 15px;">пн-пт</span>    9:00 - 19:00</p>
+            <hr>
+            <p><span style="margin-right: 40px;">сб</span>9:00 - 15:00</p>
+            <hr>
+            <p><span style="margin-right: 40px;">вс</span><span style="letter-spacing: 0.1em;">выходной</span></p>
+            <hr>
+
+            <h3>контакты</h3>
+            <p style="letter-spacing: 0.04em;"><a href="tel:0662673272" style="color: #fff;">+38  066-267-32-72</a></p>
+            <hr>‎
+            <p style="letter-spacing: 0.04em;"><a href="tel:0979289591" style="color: #fff;">+38  097-928-95-91</a></p>
+            <hr>
+            <p>ул. Херсонская, 10а</p>
+            <hr>
+            <p class="spaising07">г. Днепр, Украина</p>
+            <hr>
+<!--            <div class="social">-->
+<!--                <a href="https://ok.ru/profile/571221007200" target="_blank"><img src="img/ok_f.png" style="width: auto;"></a>-->
+<!--                <a href="https://vk.com/public136244461" target="_blank"><img src="img/vk_f.png" style="width: auto;"></a>-->
+<!--                <a href="https://www.facebook.com/dnipromedical/" target="_blank"><img src="img/fb_f.png" style="width: auto;"></a>-->
+<!--            </div>-->
+
+        </div>
+    </div>
